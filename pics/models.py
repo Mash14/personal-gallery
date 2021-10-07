a1c86c2113cc84b74e5d05feef900ad7e6398a6f
mash <mashalonzo741@gmail.com>
2021-10-07 16:20:09 +0300

    Add the location model

--- pics/models.py
+++ pics/models.py
@@ -2,4 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class Location(models.Model):
+    location = models.CharField(max_length=30,blank=True)
 
+    def __str__(self):
+        return self.location
+
+    def save_location(self):
+        self.save()
+
+    @classmethod
+    def update_location(cls,id,new_location):
+        cls.objects.filter(id).update(location = new_location)
+
+    @classmethod
+    def delete_location(cls,id):
+        cls.objects.filter(id).delete() 
