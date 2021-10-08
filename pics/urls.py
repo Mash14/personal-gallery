26905f725c723660ae0bf8eeb7ee352fa9fb2f20
mash <mashalonzo741@gmail.com>
2021-10-08 15:53:16 +0300

    Add the url to the index page

--- pics/urls.py
+++ pics/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    
+    url('^$',views.image_list,name = 'imageIndex')
 ]
 
 if settings.DEBUG:
--- pics/views.py
+++ pics/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Image,Category,Location
+
 # Create your views here.
+
+def image_list(request):
+    images = Image.image_list()
+    return render(request, 'index.html',{"images":images})
