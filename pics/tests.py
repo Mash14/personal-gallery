ebb7fe9bd68911a400ef67d7121bdd0a022fc51e
mash <mashalonzo741@gmail.com>
2021-10-08 05:39:26 +0300

    Add the category model tests

--- pics/tests.py
+++ pics/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Location
+from .models import Location,Category
 
 # Create your tests here.
 
@@ -9,6 +9,7 @@ class LocationTestCase(TestCase):
     def setUp(self):
         self.new_location = Location(location = 'Nairobi')
 
+    # tearDown method
     def tearDown(self):
         Location.objects.all().delete()
 
@@ -18,9 +19,9 @@ class LocationTestCase(TestCase):
 
     # Testing instance variables
     def test_check_instance_variables(self):
-        self.assertEquals(self.new_location.location,'Nairobi')
+        self.assertEquals(self.new_location.location, 'Nairobi')
 
-     # Test Save Method
+    # Test Save Method
     def test_save_method(self):
         self.new_location.save_location()
         locations = Location.objects.all()
@@ -29,15 +30,53 @@ class LocationTestCase(TestCase):
     # Test update method
     def test_update_method(self):
         self.new_location.save_location()
-        id = self.new_location.id
-        Location.update_location(id, 'Mombasa')
-        self.assertEqual(self.new_location.location, 'Mombasa')
+        location_id = self.new_location.id
+        Location.update_location(location_id, 'Mombasa')
+        self.assertEquals(self.new_location.location, 'Mombasa')
 
     # Test delete method
     def test_delete_method(self):
         self.new_location.save_location()
-        self.new_location.delete_location(id)
+        self.new_location.delete_location()
         location = Location.objects.all()
         self.assertTrue(len(location) == 0)
 
-     
+
+class CategoryTestClass(TestCase):
+
+    #setUp method
+    def setUp(self):
+        self.new_category = Category(category = 'Travel')
+
+    # tearDown method
+    def tearDown(self):
+        Category.objects.all().delete()
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_category, Category))
+
+    # Testing instance variables
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_category.category, 'Travel')
+
+    # Test Save Method
+    def test_save_method(self):
+        self.new_category.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0)
+
+    # Test update method
+    def test_update_method(self):
+        self.new_category.save_category()
+        category_id = self.new_category.id
+        Category.update_category(category_id, 'Food')
+        self.assertEquals(self.new_category.category, 'Food')
+
+     # Test delete method
+    def test_delete_method(self):
+        self.new_category.save_category()
+        category_id = self.new_category.id
+        self.new_category.delete_category(category_id)
+        category = Category.objects.all()
+        self.assertTrue(len(category) == 0)
